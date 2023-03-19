4c19e45d602874f13151295017cd0db249703071
Tirza <tir5566@gmail.com>
2023-03-19 12:55:01 +0200

    home page

--- frontend/src/app/components/home-area/home/home.component.ts
+++ frontend/src/app/components/home-area/home/home.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { CartModel } from 'src/app/models/cart.model';
+import { OrderModel } from 'src/app/models/order.model';
 import { ProductModel } from 'src/app/models/product.model';
 import { ItemService } from 'src/app/services/item.service';
+import { OrderService } from 'src/app/services/order.service';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -13,16 +15,19 @@ export class HomeComponent implements OnInit{
 
   public products: ProductModel[];
   public amountOfProducts:number=0;
-  public carts:CartModel[];
+  public amountOfOrders:number=0;
+  public orders:OrderModel[];
   //DI= Dependency Injection, we get object kind of service
   //angular inject object by constructor to this component
-  constructor(private productService: ProductService, private itemService:ItemService) {}
+  constructor(private productService: ProductService, private orderService:OrderService) {}
 
   public async ngOnInit() {
     try {
       this.products = await this.productService.getAllProducts();
-      this.carts= await this.itemService.getAllCarts();
-      console.log(this.carts);
+      this.orders= await this.orderService.getAllOrders();
+      console.log(this.orders);
+      this.amountOfProducts=this.products.length;
+      this.amountOfOrders=this.orders.length;
       // this.amountOfProducts=this.products.map(item=>{
       //   return this.amountOfProducts+=1;
       // })
