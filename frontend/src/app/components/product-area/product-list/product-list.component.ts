2e4bb6aa9f42080b71baea0101f29f8ba1f6e9a6
Tirza <tir5566@gmail.com>
2023-03-22 15:26:38 +0200

    design add-product component

--- frontend/src/app/components/product-area/product-list/product-list.component.ts
+++ frontend/src/app/components/product-area/product-list/product-list.component.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { ItemModel } from 'src/app/models/item.model';
 import { ProductModel } from 'src/app/models/product.model';
 import { clientStore } from 'src/app/redux/login-state';
 import { ItemService } from 'src/app/services/item.service';
 import { ProductService } from 'src/app/services/product.service';
 import { Router } from '@angular/router';
+import { itemStore } from 'src/app/redux/item-state';
+import { CategoryModel } from 'src/app/models/category.model';
 
 @Component({
   selector: 'app-product-list',
@@ -12,15 +14,42 @@ import { Router } from '@angular/router';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit{
-
+  public temp:ProductModel[]=[];
+  public isExist:boolean=true;
   public products: ProductModel[];
+  public allItems : ItemModel[];
+  public categories: CategoryModel[];
+
   //DI= Dependency Injection, we get object kind of service
   //angular inject object by constructor to this component
   constructor(private productService: ProductService, private itemService: ItemService,private router: Router) {}
 
+  buttonWidth = '85px'; // default button width
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event:any) {
+    // get the window width
+    const windowWidth = event.target.innerWidth;
+
+    // change the button width based on some logic
+    if (windowWidth < 500) {
+      this.buttonWidth = '50px';
+    } else if (windowWidth < 800) {
+      this.buttonWidth = '75px';
+    } else {
+      this.buttonWidth = '100px';
+    }
+  }
+
   public async ngOnInit() {
     try {
       this.products = await this.productService.getAllProducts();
+      this.temp=this.products;
+      this.categories=await this.productService.getAllCategory();
+      this.allItems =  itemStore.getState().items
+      itemStore.subscribe(() => {
+        this.allItems = itemStore.getState().items;
+      })
     } catch (err) {
       alert(err);
     }
@@ -41,5 +70,22 @@ export class ProductListComponent implements OnInit{
     }
   }
 
-
+  public async sortByCategory(data:any) {
+    //this.products = await this.productService.getAllProducts();
+    console.log("function is working",data)
+    if(data=='all'){
+      this.temp=this.products;
+    }else{
+      this.temp = await this.productService.getProductsByCategory(data);
+    }
+  }
+  public searchItem(data:string){
+    this.temp=this.products.filter(item=>{return item.name.includes(data)});
+    console.log(this.temp.length)
+    if(this.temp.length==0){
+      this.isExist=false;
+    }else{
+      this.isExist=true;
+    }
+  }
 }
